51d11c2dbf532c2338072442b045eed1c26b9be1
Baptiste <mail@baptistebillet.com>
2015-11-25 17:12:05 +0100

    Base

---
+++ Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotate : MonoBehaviour {
+
+    public float m_Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(transform.forward, Time.deltaTime * m_Speed);
+	}
+}
---
+++ Assets/Scripts/WebCamPhotoCamera.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class WebCamPhotoCamera : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+    public Renderer m_RendererPhoto;
+    public Renderer m_RendererRendu;
+
+    bool m_PhotoMode=true;
+
+    Texture2D photo;
+
+
+    void Start()
+    {
+
+        webCamTexture = new WebCamTexture("Camera 1");
+        //webCamTexture = new WebCamTexture();
+
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+
+        m_RendererPhoto.material.mainTexture = webCamTexture;
+        
+         //StartVideo
+        webCamTexture.Play();
+
+        
+        //Set Mode
+        m_PhotoMode = true;
+        m_RendererPhoto.gameObject.SetActive(m_PhotoMode);
+        m_RendererRendu.gameObject.SetActive(!m_PhotoMode);
+
+
+
+    }
+
+    //Take the picture and change mode
+    void TakePhoto()
+    {
+
+        photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+
+        photo.SetPixels(webCamTexture.GetPixels());
+
+        photo.Apply();
+
+
+        /*
+        //Encode to a PNG
+        byte[] bytes = photo.EncodeToPNG();
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes(your_path + "photo.png", bytes);
+        */
+        DisplayPhoto();
+    }
+
+    //Display Picture
+    void DisplayPhoto()
+    {
+        m_RendererRendu.material.mainTexture = photo;
+
+    }
+
+    void ChangeMode()
+    {
+        if (m_PhotoMode)
+        {
+            TakePhoto();
+        }
+
+
+        m_PhotoMode = !m_PhotoMode;
+
+        m_RendererPhoto.gameObject.SetActive(m_PhotoMode);
+        m_RendererRendu.gameObject.SetActive(!m_PhotoMode);
+
+    }
+
+   
+
+    void OnMouseDown()
+    {
+
+        ChangeMode();
+    }
+   
+   
+}
