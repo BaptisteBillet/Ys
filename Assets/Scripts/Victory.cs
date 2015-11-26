ecc7a57c7bd7b9d4209965b2945041763baee12c
Baptiste <mail@baptistebillet.com>
2015-11-27 00:09:07 +0100

    Victory

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Victory : MonoBehaviour {
+
+    public Color m_LionColor;
+    public Color m_BearColor;
+
+    public Sprite m_LionSprite;
+    public Sprite m_BearSprite;
+
+    public Image m_ImageWinner;
+    public Image m_BackWinner;
+
+
+    void IsLionWin(bool isLionWin)
+    {
+        if(isLionWin)
+        {
+            m_ImageWinner.sprite = m_LionSprite;
+            m_BackWinner.color = m_LionColor;
+        }
+        else
+        {
+            m_ImageWinner.sprite = m_BearSprite;
+            m_BackWinner.color = m_BearColor;
+        }
+    }
+}
