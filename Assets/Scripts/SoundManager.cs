a5c76c8018853d881318b2545271df3254e9517c
Baptiste <mail@baptistebillet.com>
2015-11-27 00:50:59 +0100

    SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SoundManager : MonoBehaviour {
+	#region Members
+
+	[Header("MUSICS")]
+	public List<AudioClip> Music = new List<AudioClip>();
+
+	[Header("SOUNDS")]
+	public List<AudioClip> Sound= new List<AudioClip>();
+	
+	[Header("LIONROAR")]
+	public List<AudioClip> LionRoar = new List<AudioClip>();
+
+    [Header("BEARROAR")]
+    public List<AudioClip> BearRoar = new List<AudioClip>();
+
+    [Header("Sound Listeners")]
+	public List<AudioSource> Source = new List<AudioSource>();
+
+
+	#endregion
+
+	// Use this for initialization
+	void Start()
+	{
+		SoundManagerEvent.onEvent += Play;
+	}
+
+	void OnDestroy()
+	{
+		SoundManagerEvent.onEvent -= Play;
+	}
+
+	public void Play(SoundManagerType emt)
+	{
+		switch (emt)
+		{
+			case SoundManagerType.ABSOLUMENT:
+				Source[2].Stop();
+				Source[2].clip = Music[0];
+				Source[2].Play();
+				break;
+		}
+	}
+
+	
+
+}
---
+++ Assets/Scripts/SoundManagerEvent.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Comment émettre un event:
+		SoundManagerEvent.emit(EventManagerType.ENEMY_HIT);
+ * 
+ * Comment traiter un event (dans un start ou un initialisation)
+		EventManagerScript.onEvent += (EventManagerType emt, GameObject go) => {
+			if (emt == EventManagerType.ENEMY_HIT)
+			{
+				//SpawnFXAt(go.transform.position);
+			}
+		};
+ * ou:
+		void maCallback(EventManagerType emt, GameObject go) => {
+			if (emt == EventManagerType.ENEMY_HIT)
+			{
+				//SpawnFXAt(go.transform.position);
+			}
+		};
+		EventManagerScript.onEvent += maCallback;
+ * 
+ * qui permet de:
+		EventManagerScript.onEvent -= maCallback; //Retire l'appel
+ */
+
+
+public enum SoundManagerType
+{
+	ABSOLUMENT,
+	BIENJOUE,
+	BONBOULOT,
+	BRAVO,
+	EXCELLENT,
+	SUPER,
+	AIEAIEAIE1,
+	AIEAIEAIE2,
+	AIEAIEAIE3,
+	CENESTPASCA,
+	NONNONNON1,
+	NONNONNON2,
+	PERDU,
+	PRESQUE,
+	TUFERASMIEUX,
+	RANDOMPOSITIVE,
+	RANDOMNEGATIVE,
+	TURMEL00,
+	TURMEL01,
+	TURMEL02,
+	TURMEL03,
+	TURMEL04,
+	TURMEL05,
+	TURMEL06,
+	TURMEL07,
+	TURMEL08,
+	TURMEL09,
+	TURMEL10,
+	MISHOOOO1,
+	MISHOOOO2
+}
+
+public class SoundManagerEvent : MonoBehaviour
+{
+
+	public delegate void EventAction(SoundManagerType emt);
+	public static event EventAction onEvent;
+
+	#region Singleton
+	static private SoundManagerEvent s_Instance;
+	static public SoundManagerEvent instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+	
+
+
+	void Awake()
+	{
+		if (s_Instance == null)
+			s_Instance = this;
+		//DontDestroyOnLoad(this);
+	}
+    #endregion
+    void Start()
+	{
+		SoundManagerEvent.onEvent += (SoundManagerType emt) => { Debug.Log(""); };
+	}
+
+	public static void emit(SoundManagerType emt)	{
+		
+		if (onEvent != null)
+		{
+			onEvent(emt);
+		}
+	}
+
+
+
+}
