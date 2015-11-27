346d0713b2a76151f4682a471c9717df13d33d6f
Baptiste <mail@baptistebillet.com>
2015-11-27 12:13:50 +0100

    Victory

--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -13,6 +13,9 @@ public class Victory : MonoBehaviour {
     public Image m_ImageWinner;
     public Image m_BackWinner;
 
+	public Animator m_Animator;
+
+	private bool m_ReadyToGo=false;
 
     public void IsLionWin(bool isLionWin)
     {
@@ -26,5 +29,21 @@ public class Victory : MonoBehaviour {
             m_ImageWinner.sprite = m_BearSprite;
             m_BackWinner.color = m_BearColor;
         }
+
+		m_Animator.SetTrigger ("Open");
+
     }
+
+	public void OkToGo()
+	{
+		m_ReadyToGo = true;
+
+	}
+
+
+	void OnMouseDown()
+	{
+		m_Animator.SetTrigger ("Close");
+	}
+
 }
